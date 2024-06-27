f61c8c08a8505564e564f8409cb7ee6a5b457335
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2024-06-27 17:47:09 +0900

    feat: Database name, password 환경변수로 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.2.2")
     implementation("org.mariadb.jdbc:mariadb-java-client")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/com/example/kotlin_crud/KotlinCrudApplication.kt
+++ src/main/kotlin/com/example/kotlin_crud/KotlinCrudApplication.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin_crud
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,8 @@ import org.springframework.boot.runApplication
 class KotlinCrudApplication
 
 fun main(args: Array<String>) {
+    val dotenv = Dotenv.configure().load()
+    System.setProperty("DB_USERNAME",dotenv["DB_USERNAME"])
+    System.setProperty("DB_PASSWORD",dotenv["DB_PASSWORD"])
     runApplication<KotlinCrudApplication>(*args)
 }
